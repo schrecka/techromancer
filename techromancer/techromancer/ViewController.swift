594b8aa1556ef207bf551afb7ec3aab10a6ea285
schrecka <schrecka@umich.edu>
2019-07-19 15:54:52 +0100

    cocoa app

---
+++ techromancer/techromancer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  techromancer
+//
+//  Created by Adam Schreck on 7/19/19.
+//  Copyright © 2019 Adam Schreck. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
