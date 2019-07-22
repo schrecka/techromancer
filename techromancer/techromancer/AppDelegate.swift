35b52347931970597994959a590dd23ea74f0ce8
schrecka <schrecka@umich.edu>
2019-07-22 17:25:56 +0100

    deleted extra files

--- techromancer/techromancer/AppDelegate.swift
+++ techromancer/techromancer/AppDelegate.swift
@@ -11,16 +11,31 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    //from https://www.raywenderlich.com/450-menus-and-popovers-in-menu-bar-apps-for-macos#toc-anchor-001
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
+    
+    @objc func printQuote(_ sender: Any?) {
+        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+        let quoteAuthor = "Mark Twain"
+        
+        print("\(quoteText) — \(quoteAuthor)")
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
+            button.action = #selector(printQuote(_:))
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-
+    
+  
+    
+   
 }
 
