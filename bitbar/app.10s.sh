7d41d26ad9717b62cb2227dae68304fbfe07da9e
schrecka <schrecka@umich.edu>
2019-07-29 15:01:32 +0100

    started working on bitbar

---
+++ bitbar/app.10s.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#chmod u+x app.1m.sh
+
+#metadata
+# <bitbar.title>techromancer</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Adam and Ari</bitbar.author>
+# <bitbar.author.github>schrecka</bitbar.author.github>
+# <bitbar.desc>fixme</bitbar.desc>
+# <bitbar.image>http://www.hosted-somewhere/pluginimage</bitbar.image>
+# <bitbar.dependencies>fixme</bitbar.dependencies>
+# <bitbar.abouturl>techromancer.ml</bitbar.abouturl>
+
+#date
+
+echo "techromancer"
+echo "---"
+
+
+chmod u+x /Users/Adam/Desktop/techromancer/techromancer/bitbar/scripts/watcher-test.py
+/usr/local/bin/python3 /Users/Adam/Desktop/techromancer/techromancer/bitbar/scripts/watcher-test.py
+
+echo "Four"
+echo "Five"
+echo "Six"
