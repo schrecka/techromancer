72c636b28aa5d9dae6fcba01e4642bbfbbb87b9d
schrecka <schrecka@umich.edu>
2019-07-30 14:37:52 +0100

    python instead of bash bitbar

---
+++ bitbar/app.1m.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#metadata
+# <bitbar.title>techromancer</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Adam and Ari</bitbar.author>
+# <bitbar.author.github>schrecka</bitbar.author.github>
+# <bitbar.desc>fixme</bitbar.desc>
+# <bitbar.image>http://www.hosted-somewhere/pluginimage</bitbar.image>
+# <bitbar.dependencies>fixme</bitbar.dependencies>
+# <bitbar.abouturl>techromancer.ml</bitbar.abouturl>
+
+print ("techromancer")
+print ("---")
+print ("running color=blue")
--- bitbar/scripts/run.sh
+++ bitbar/scripts/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #chmod u+x app.1m.sh
-echo "open" > /Users/Adam/Desktop/techromancer/techromancer/bitbar/scripts/is_open.txt
-chmod u+x /Users/Adam/Desktop/techromancer/techromancer/bitbar/scripts/watcher-test.py
-chmod +x /Users/Adam/Desktop/techromancer/techromancer/bitbar/scripts/logger-test.txt
+echo "open" >> /Users/Adam/Desktop/techromancer/techromancer/bitbar/scripts/is_open.txt
+echo "this be working"
+# chmod u+x /Users/Adam/Desktop/techromancer/techromancer/bitbar/scripts/watcher-test.py
+# chmod +x /Users/Adam/Desktop/techromancer/techromancer/bitbar/scripts/logger-test.txt
